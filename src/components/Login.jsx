240a355e099ac94226044d07e8b3ac7b98d252a6
Paul Leandro Lanot <kraft091@gmail.com>
2023-03-08 10:09:33 +0800

    deploy google

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import { GoogleLogin } from '@react-oauth/google';
 const Login = () => {
-  return <div>Login</div>;
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
+  return (
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+    </div>
+  );
 };
 
 export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="951040486378-ufng73otli4mmisk4eo28svorsv93u3t.apps.googleusercontent.com">
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
